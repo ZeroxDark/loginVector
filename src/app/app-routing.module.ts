40ad621facebd7d7c9be33a9b1e1a7145a31dcf2
Kevin Candia Villagomez <kacandia@vectoritcgroup.com>
2018-06-12 17:18:42 -0500

    rutas de aplicativo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { PrivatePageComponent } from './componentes/private-page/private-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'register',
+    component: LoginPageComponent
+  },
+  {
+    path: 'privado',
+    component: PrivatePageComponent
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
