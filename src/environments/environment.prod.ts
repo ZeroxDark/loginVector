772f73f31ef4319ae9571546d927a2963e5e4b56
Kevin Candia Villagomez <kacandia@vectoritcgroup.com>
2018-06-21 16:56:09 -0500

    push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyASZZDrbz5O0xolw3gvkscuzA6xZUqhyzI",
-    authDomain: "loginvector-550e2.firebaseapp.com",
-    databaseURL: "https://loginvector-550e2.firebaseio.com",
-    projectId: "loginvector-550e2",
-    storageBucket: "loginvector-550e2.appspot.com",
-    messagingSenderId: "523735166072"
-  }
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,5 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyASZZDrbz5O0xolw3gvkscuzA6xZUqhyzI",
-    authDomain: "loginvector-550e2.firebaseapp.com",
-    databaseURL: "https://loginvector-550e2.firebaseio.com",
-    projectId: "loginvector-550e2",
-    storageBucket: "loginvector-550e2.appspot.com",
-    messagingSenderId: "523735166072"
-  }
+ 
 };
